d49296f760f9714ebcc5c7ae2ca25a54dd404ddb
Manik Maity <manikmaity156@gmail.com>
2025-01-17 15:45:27 +0530

    Home loader added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,22 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-import useCreateWorkspaceContext from "@/hooks/apis/context/useCreateWorkspaceContext";
-import useGetUserWorkspace from "@/hooks/apis/workspaces/useGetUserWorkspace";
+import useAuthContext from "@/hooks/apis/context/useAuthContext";
+import { toast } from "@/hooks/use-toast";
+import JumpLoader from "@/components/atoms/Loader/JumpLoader";
 
 function Home() {
-    const { workspacesData, isError, isLoading } =
-    useGetUserWorkspace();
-  const navigate = useNavigate();
-  const { setOpenCreateModal } = useCreateWorkspaceContext();
+  const { auth } = useAuthContext();
 
-  useEffect(() => {
-    if (isLoading || isError) return;
-    if (workspacesData?.data?.length === 0 || !workspacesData?.data){
-      setOpenCreateModal(true);
-    }
-    else {
-      navigate(`/workspace/${workspacesData?.data[0]?._id}/channel/${workspacesData?.data[0]?.channels[0]}`);
-    }
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [workspacesData, isLoading, isError]);
-
-  return <div>Wokspaces</div>;
+  if (auth.loading) {
+    return <div className="h-screen w-screen grid place-content-center"><JumpLoader/></div>;
+  } else if (auth.user && auth.token) {
+    return <Navigate to="/workspaces" />;
+  } else {
+    toast({
+      description: "Please signin first to access your workspace.",
+    });
+    return <Navigate to="/signin" />;
+  }
 }
 
 export default Home;
