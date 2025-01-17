c3e296a47bfb7e5ac96e88e58be4d77961f02c18
Manik Maity <manikmaity156@gmail.com>
2025-01-17 14:32:27 +0530

    Changed hardcoded url to env

--- src/config/clientConfig.js
+++ src/config/clientConfig.js
@@ -1,4 +1,5 @@
 export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const BACKEND_SOCKET_URL = import.meta.env.VITE_BACKEND_SOCKET_URL;
 export const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
 export const RAZORPAY_ID = import.meta.env.VITE_RAZORPAY_ID;
